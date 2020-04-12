3ad5ac4cdb2006cd917285caa7ad38fa4cf83c93
ivanaro <ivanaro@DESKTOP-L58TPAI>
2020-04-12 19:54:02 +0200

    Primer Commit

---
+++ proyectoEntornosGrupo1/src/proyectoEntornosGrupo/Hola_Mundo.java
@@ -0,0 +1,11 @@
+package proyectoEntornosGrupo;
+
+public class Hola_Mundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Somo el grupo 1 de Entornos");
+	}
+
+}
